38f0c2da6d098a5612d163dfceefed16ceea2d80
ipukhonin <ipukhonin@gmail.com>
2021-10-15 16:50:49 +0300

    AIRPORT-12 Added base Spring Security Up to 0.9.5-SNAPSHOT Added new logic in TicketsService for passengerId check

--- src/main/java/com/app/airport/service/TicketsService.java
+++ src/main/java/com/app/airport/service/TicketsService.java
@@ -3,6 +3,7 @@ package com.app.airport.service;
 import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Random;
 import com.app.airport.dto.BookingDto;
 import com.app.airport.dto.TicketDto;
 import com.app.airport.entity.Ticket;
@@ -40,6 +41,7 @@ public class TicketsService {
 
   @Transactional(value = Transactional.TxType.REQUIRED)
   public void saveNewTicket(TicketDto ticketDto) {
+    checkAndGeneratePassId(ticketDto);
     saveTicket(mapTicketEntityFromDto(ticketDto));
     bookingsService.saveBookings(ticketDto.getBooking());
   }
@@ -72,5 +74,53 @@ public class TicketsService {
 
   private Ticket mapTicketEntityFromDto(TicketDto ticketDto) {
     return mapper.mapDtoToEntity(ticketDto);
+  }
+
+  private void checkAndGeneratePassId(TicketDto ticketDto) {
+    boolean isExist = checkIfPassengerIdIsPresentInRepository(ticketDto.getPassengerId());
+    if (isExist) {
+      generatePassId(isExist, ticketDto);
+    }
+  }
+
+  private void generatePassId(boolean isExist, TicketDto ticketDto) {
+    String passengerId;
+    while (isExist) {
+      passengerId = getPassengerId();
+      isExist = checkIfPassengerIdIsPresentInRepository(passengerId);
+      if (!isExist) {
+        ticketDto.setPassengerId(passengerId);
+        break;
+      }
+    }
+  }
+
+  private boolean checkIfPassengerIdIsPresentInRepository(String passengerId) {
+    return repository.findTicketsByPassengerId(passengerId).stream().findFirst().isPresent();
+  }
+
+  private String getPassengerId() {
+    return getNumber(4) + " " + getNumber(6);
+  }
+
+  /**
+   * This method can generate number with 10 chars max (Integer type constraints).
+   *
+   * @param chars chars in result number
+   * @return String value of generated number
+   */
+  private String getNumber(Integer chars) {
+    int rndLimiter = (int) Math.round(Math.pow(10, chars) - 1);
+    Random rnd = new Random();
+    String number = String.valueOf(rnd.nextInt(rndLimiter));
+    if (number.length() < chars) {
+      int missingInt = chars - number.length();
+      StringBuilder finalNumberBuilder = new StringBuilder(number);
+      for (int i = 0; i < missingInt; i++) {
+        finalNumberBuilder.insert(0, "0");
+        number = finalNumberBuilder.toString();
+      }
+    }
+    return number;
   }
 }
